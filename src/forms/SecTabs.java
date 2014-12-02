6cb779b17f2689ea9b3c8711ed1069d247624e9c
Hugo Young <hybas3@gmail.com>
2014-12-02 11:33:32 +0000

    alternative layout for the interface

---
+++ src/forms/SecTabs.java
@@ -0,0 +1,54 @@
+package DBTEST04;
+
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SecTabs extends JFrame {
+	
+	public SecTabs () {
+		initUI();
+	}
+	
+	private void initUI() {
+		JFrame frame = new JFrame();
+		JTabbedPane tabbedPane = new JTabbedPane();
+		String[] tabText = {
+			"View Appointments",
+			"Book Appointment",
+			"Subscribe Patient",
+			"Review Treatments",
+			"Modify Owed",
+			"Register Patient",
+			"Register Address"
+		};
+		JComponent[] tabComponents = {
+			null,
+			new BookAppointment(frame),
+			new SubscribePatient(frame),
+			null,
+			null,
+			new RegisterPatient(frame),
+			new RegisterAddress(frame),
+		};
+		for (int i=0; i<tabText.length; i++) {
+			tabbedPane.addTab(tabText[i], tabComponents[i]);
+		}
+		
+		add(tabbedPane);
+		
+		setTitle("place-holder");
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		JFrame panel = new SecTabs();
+		panel.setVisible(true);
+	}
+
+}
