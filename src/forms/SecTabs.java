d013ab929c49b14bd5d751e0d087cc08924432ba
Hugo Young <hybas3@gmail.com>
2014-12-02 11:35:15 +0000

    removed useless imports

--- src/forms/SecTabs.java
+++ src/forms/SecTabs.java
@@ -2,10 +2,6 @@ package DBTEST04;
 
 
 import javax.swing.*;
-import java.util.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class SecTabs extends JFrame {
 	
