32a8de1e53da068721f78eaa9f9a7319643de3c9
Hugo <hybas3@gmail.com>
2014-12-03 14:10:20 +0000

    QueryHandler with only 1 driver load

--- src/forms/QueryHandler.java
+++ src/forms/QueryHandler.java
@@ -1,4 +1,4 @@
-package forms;
+package DBTEST06;
 
 import java.util.logging.Logger;
 import java.sql.*;
@@ -9,17 +9,22 @@ public class QueryHandler{
     public QueryHandler(String uname, String pwd){
         this.usr = uname;
         this.pwd = pwd;
-    }
-    
-	public Object[] executeQueryRS(String query) throws 
-    	InstantiationException, IllegalAccessException{
-		ResultSet rs = null;
         try {
     		Class.forName("com.mysql.jdbc.Driver").newInstance();
     	} catch (ClassNotFoundException e) {
     		throw new RuntimeException("Can't find driver", e);
-    	}
+    	} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         LOGGER.info("Loaded driver.");
+    }
+    
+	public Object[] executeQueryRS(String query) {
+		ResultSet rs = null;
         Connection conn = null;
 		try {
     		conn = DriverManager.getConnection
@@ -48,14 +53,8 @@ public class QueryHandler{
         ResultSet rs = null;
 
         LOGGER.info("Getting result set.");
-        try{
-        	connectionObjs = executeQueryRS(qri);
-            rs = (ResultSet)connectionObjs[1];
-        }catch(InstantiationException i){
-            throw new RuntimeException("Instantiation ex thrown.", i);
-        }catch(IllegalAccessException i){
-            throw new RuntimeException("Illegal access ex thrown.", i);
-        }
+        connectionObjs = executeQueryRS(qri);
+		rs = (ResultSet)connectionObjs[1];
         LOGGER.info("Got result set."); 
         String[] returnStr = null;
         try{
@@ -97,16 +96,6 @@ public class QueryHandler{
     }
 
     public int executeUpdate(String updt){
-        //Load driver.
-        try {
-    		Class.forName("com.mysql.jdbc.Driver").newInstance();
-    	}catch(ClassNotFoundException e){
-    		throw new RuntimeException("Can't find driver", e);
-    	}catch(InstantiationException i){
-            throw new RuntimeException("Instantiation error thrown.", i);
-        }catch(IllegalAccessException i){
-            throw new RuntimeException("Illegal access ex thrown.", i);
-        }
         //Connect to database.
         int updtStatus = -1;
         Connection conn = null;
@@ -146,14 +135,8 @@ public class QueryHandler{
         int noCols = 0;
 
         LOGGER.info("Getting result set.");
-        try{
-        	connectionObjs = executeQueryRS(qri);
-            rs = (ResultSet)connectionObjs[1];
-        }catch(InstantiationException i){
-            throw new RuntimeException("Instantiation ex thrown.", i);
-        }catch(IllegalAccessException i){
-            throw new RuntimeException("Illegal access ex thrown.", i);
-        }
+        connectionObjs = executeQueryRS(qri);
+		rs = (ResultSet)connectionObjs[1];
         LOGGER.info("Got result set."); 
         ArrayList<String[]> AryReturnStr = new ArrayList<String[]>();
         try{
